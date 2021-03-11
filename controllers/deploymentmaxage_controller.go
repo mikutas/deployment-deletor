2a543d079091ed799bd5488ae58fef4981fbd929
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2021-03-11 18:39:49 +0900

    fix: add verbs
    暗黙的に必要とされていてエラーが出るため
    E0311 09:38:01.478978       1 reflector.go:307] pkg/mod/k8s.io/client-go@v0.17.2/tools/cache/reflector.go:105: Failed to watch *v1.Namespace: unknown (get namespaces)
    E0311 09:38:01.579023       1 reflector.go:153] pkg/mod/k8s.io/client-go@v0.17.2/tools/cache/reflector.go:105: Failed to list *v1.Deployment: deployments.apps is forbidden: User "system:serviceaccount:laboratory:default" cannot list resource "deployments" in API group "apps" at the cluster scope

--- controllers/deploymentmaxage_controller.go
+++ controllers/deploymentmaxage_controller.go
@@ -42,8 +42,8 @@ type DeploymentMaxAgeReconciler struct {
 
 // +kubebuilder:rbac:groups=tkms0106.example.com,resources=deploymentmaxages,verbs=get;list;watch;update;patch
 // +kubebuilder:rbac:groups=tkms0106.example.com,resources=deploymentmaxages/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups="",resources=namespaces,verbs=list
-// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;delete
+// +kubebuilder:rbac:groups="",resources=namespaces,verbs=list;watch
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;delete
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
 
 func (r *DeploymentMaxAgeReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
