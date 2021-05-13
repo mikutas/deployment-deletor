f9158eacfa3d08f12dad7b194761512761f20ad7
mikutas <23391543+mikutas@users.noreply.github.com>
2021-05-13 17:29:11 +0900

    build: kubebuilder create api
    --group mikutas --version v1alpha1 --kind DeploymentDeletor

--- main.go
+++ main.go
@@ -90,6 +90,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DeploymentMaxAge")
 		os.Exit(1)
 	}
+	if err = (&controllers.DeploymentDeletorReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("DeploymentDeletor"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DeploymentDeletor")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
